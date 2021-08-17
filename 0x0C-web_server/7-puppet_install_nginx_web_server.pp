362b1956a130c61abe10e017a23e38c377a33407
EugeneJoe <jmuthui70@gmail.com>
2021-08-17 20:42:11 +0000

    Update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,14 +1,10 @@
 # Install and configure an Nginx server with the following requirements:
 # Listens on port 80
-# When querying Nginx at its root '/', return a page containing the string 'Holberton School'
-# Perform a permanent redirect when you query Nginx at '/redirect_me'
+# When querying Nginx at its root /, return a page containing the string Holberton School
+# Perform a permanent redirect when you query Nginx at /redirect_me
 
 package { 'nginx':
       ensure => installed,
-}
-
-file { '/var/www/html/index.html':
-      content => 'Holberton School',
 }
 
 file_line { 'rewrite redirect':
@@ -16,6 +12,10 @@ file_line { 'rewrite redirect':
     path   => '/etc/nginx/sites-available/default',
     after  => 'server_name _;',
     line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+      content => 'Holberton School',
 }
 
 service { 'nginx':
