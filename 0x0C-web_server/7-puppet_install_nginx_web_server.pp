e8959e40bc94ca77404686b5fa99da242a3a4156
EugeneJoe <jmuthui70@gmail.com>
2021-08-17 19:50:34 +0000

    Update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,14 +4,20 @@
 # When querying Nginx at its root '/', return a page containing the string 'Holberton School'
 # Perform a permanent redirect when you query Nginx at '/redirect_me'
 
+exec { 'update':
+    command => 'sudo apt-get -y update',
+    path    => '/usr/bin:/usr/sbin:/bin',
+}
+
 package { 'nginx':
       ensure   => 'installed',
       name     => 'nginx',
       provider => 'apt',
+      require  => Exec['update'],
 }
 
 exec { 'ufw allow':
-      command => 'sudo ufw allow "Nginx HTTP"',
+      command => "sudo ufw allow \'Nginx HTTP\'",
       path    => '/usr/bin:/usr/sbin:/bin',
 }
 
@@ -20,16 +26,19 @@ file { '/var/www/html/index.nginx-debian.html':
       path    => '/var/www/html/index.nginx-debian.html',
       mode    => '0644',
       owner   => 'root',
+      group   => 'root',
       content => 'Holberton School',
 }
 
 file_line { 'rewrite redirect':
-    path  => '/etc/nginx/sites-available/default',
-    match => "^\tserver_name _;",
-    line  => "\tserver_name _;\n\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;",
+    ensure  => 'present',
+    path    => '/etc/nginx/sites-available/default',
+    match   => "^\tserver_name _;",
+    line    => "\tserver_name _;\n\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;",
 }
 
 exec { 'nginx restart':
     command => 'sudo service nginx restart',
     path    => '/usr/bin:/usr/sbin:/bin',
+    require => Package['nginx'],
 }
