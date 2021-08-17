5caf072fe09a526cd359794fa3325d7b83dafebd
EugeneJoe <jmuthui70@gmail.com>
2021-08-17 20:58:22 +0000

    Update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,17 +8,20 @@ package { 'nginx':
 }
 
 file_line { 'rewrite redirect':
-    ensure => 'present',
-    path   => '/etc/nginx/sites-available/default',
-    after  => 'listen 80 default_server;',
-    line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+    ensure  => 'present',
+    path    => '/etc/nginx/sites-available/default',
+    after   => 'listen 80 default_server;',
+    line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
 }
 
-file { '/var/www/html/index.html':
+file { '/var/www/html/index.nginx-debian.html':
       content => 'Holberton School',
+      require => Package['nginx'],
 }
 
 service { 'nginx':
     ensure  => 'running',
-    require => Package['nginx'],
+    require => file_line['rewrite redirect'],
 }
