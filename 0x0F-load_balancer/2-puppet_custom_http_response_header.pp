b386d8bd18f1a1ab5da8fd77ff317a97dd85a523
EugeneJoe <jmuthui70@gmail.com>
2021-08-23 22:05:03 +0000

    Update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -12,7 +12,7 @@ file_line { 'add_header':
 	  ensure  => 'present',
 	  path    => '/etc/nginx/sites-available/default;',
 	  after	  => "root /var/www/html;",
-	  line	  => "add_header X-Served-By \$hostname};",
+	  line	  => "add_header X-Served-By $hostname;",
 	  require => Package['nginx'],
 }
 
