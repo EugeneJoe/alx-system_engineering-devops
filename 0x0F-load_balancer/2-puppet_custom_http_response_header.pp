dcb908a4401ac2566aab2176cffe9f985a83c8bb
EugeneJoe <jmuthui70@gmail.com>
2021-08-23 21:14:38 +0000

    Puppet Manifest to configure custom Nginx Response Header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,19 @@
+# Install an Nginx server and add a custom header to its response headers
+
+package { 'nginx':
+	ensure => installed,
+}
+
+file_line { 'add_header':
+	  ensure  => 'present',
+	  path    => '/etc/nginx/sites-available/default',
+	  after	  => '# include snippets/snakeoil.conf;',
+	  line	  => '\\\tadd_header $hostname;',
+	  require => Package['nginx'],
+	  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+	ensure	=> 'running',
+	require	=> 'file_line['add_header'],
+}
