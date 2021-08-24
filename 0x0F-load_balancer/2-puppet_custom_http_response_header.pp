54e25c06b5dbad9f88a5db3798b9957d67f0d747
EugeneJoe <jmuthui70@gmail.com>
2021-08-24 09:47:12 +0000

    Update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -17,6 +17,14 @@ file_line { 'add_header':
   require => Package['nginx'],
 }
 
+file_line { 'redirect':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;',
+  require => Package['nginx'],
+}
+
 file { '/var/www/html/index.nginx-debian.html':
   content => 'Holberton School',
   require => Package['nginx'],
