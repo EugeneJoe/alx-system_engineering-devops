3b7d2ad772c2e0260891c4cb0223612317b44b24
EugeneJoe <jmuthui70@gmail.com>
2021-09-13 14:38:02 +0000

    Fix pep8 errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,8 @@ from sys import argv
 
 if __name__ == "__main__":
     url1 = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-    url2 = "https://jsonplaceholder.typicode.com/users/{}/todos".format(argv[1])
+    url2 = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        argv[1])
     user = requests.get(url1)
     todo = requests.get(url2)
     user = json.loads(user.text)
@@ -19,11 +20,11 @@ if __name__ == "__main__":
     tasks = []
     for item in todo:
         total += 1
-        if item['completed'] == True:
+        if item['completed']:
             done += 1
             tasks.append(item['title'])
     first = "Employee {} is done with tasks({}/{}):".format(user['name'],
-                                                           done, total)
+                                                            done, total)
     print(first)
     for item in tasks:
         print("\t {}".format(item))
