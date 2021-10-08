3d31c32ca4752befdb548d0567c9ee10f0ab8082
EugeneJoe <jmuthui70@gmail.com>
2021-10-08 09:32:22 +0000

    Update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,41 @@
-#use sed to update config files to allow nginx handle opening many files
+#use sed to update config files
 
-exec { 'fix--for-nginx':
+exec { '/etc/nginx/nginx.conf_0':
   path    => ['usr/local/sbin:usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
-  command => "sed -i '/error_log.*/a \\\topen_file_cache max=200000 inactive=20s;' /etc/nginx/nginx.conf &&\
-             sed -i '/error_log.*/a \\\topen_file_cache_valid 30s;' /etc/nginx/nginx.conf &&\
-             sed -i '/error_log.*/a \\\topen_file_cache_min_uses 2;' /etc/nginx/nginx.conf &&\
-             sed -i '/error_log.*/a \\\topen_file_cache_errors on;' /etc/nginx/nginx.conf",
+  command => "sed -i 's/worker_processes.*/worker_processes auto;/g' /etc/nginx/nginx.conf",
 }
 
-exec { 'worker_processes':
-  path    => ['usr/local/sbin:usr/local/bin:/usr/sbin://usr/bin:/sbin:/bin'],
-  command => "sed -i 's/worker_processes.*/worker_processes auto;/g' /etc/nginx/nginx.conf &&\
-  	     sed -i 's/worker_connections.*/worker_connections 4000;/g' /etc/nginx/nginx.conf &&\
-	     sed -i 's/keepalive_timeout.*/keepalive_timeout 15;/g' /etc/nginx/nginx.conf &&\
-	     sed -i 's/access_log.*/access_log off;/g' /etc/nginx/nginx.conf",
+exec { '/etc/nginx/nginx.conf_1':
+  path    => ['usr/local/sbin:usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command => "sed -i 's/worker_connections.*/worker_connections 4000;/g' /etc/nginx/nginx.conf",
+} ->
+
+exec { '/etc/nginx/nginx.conf_2':
+  path    => ['usr/local/sbin:usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command => "sed -i 's/keepalive_timeout.*/keepalive_timeout 15;/g' /etc/nginx/nginx.conf",
+} ->
+
+exec { '/etc/nginx/nginx.conf_3':
+  path    => ['usr/local/sbin:usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command => "sed -i 's/access_log.*/access_log off;/g' /etc/nginx/nginx.conf",
+} ->
+
+exec { '/etc/nginx/nginx.conf_4':
+  path    => ['usr/local/sbin:usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command => "sed -i '/error_log.*/a \\\topen_file_cache_errors on;' /etc/nginx/nginx.conf",
+}
+
+exec { '/etc/nginx/nginx.conf_5':
+  path    => ['usr/local/sbin:usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command => "sed -i '/error_log.*/a \\\topen_file_cache_min_uses 2;' /etc/nginx/nginx.conf",
+} ->
+
+exec { '/etc/nginx/nginx.conf_6':
+  path    => ['usr/local/sbin:usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command => "sed -i '/error_log.*/a \\\topen_file_cache_valid 30s;' /etc/nginx/nginx.conf",
+} ->
+
+exec { '/etc/nginx/nginx.conf_7':
+  path    => ['usr/local/sbin:usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command => "sed -i '/error_log.*/a \\\topen_file_cache max=200000 inactive=20s;' /etc/nginx/nginx.conf",
 }
