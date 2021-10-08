3bed99fc8d75d411738121f57cfdf943e2f05e0b
EugeneJoe <jmuthui70@gmail.com>
2021-10-08 09:25:22 +0000

    Update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -7,3 +7,11 @@ exec { 'fix--for-nginx':
              sed -i '/error_log.*/a \\\topen_file_cache_min_uses 2;' /etc/nginx/nginx.conf &&\
              sed -i '/error_log.*/a \\\topen_file_cache_errors on;' /etc/nginx/nginx.conf",
 }
+
+exec { 'worker_processes':
+  path    => ['usr/local/sbin:usr/local/bin:/usr/sbin://usr/bin:/sbin:/bin'],
+  command => "sed -i 's/worker_processes.*/worker_processes auto;/g' /etc/nginx/nginx.conf &&\
+  	     sed -i 's/worker_connections.*/worker_connections 4000;/g' /etc/nginx/nginx.conf &&\
+	     sed -i 's/keepalive_timeout.*/keepalive_timeout 15;/g' /etc/nginx/nginx.conf &&\
+	     sed -i 's/access_log.*/access_log off;/g' /etc/nginx/nginx.conf",
+}
